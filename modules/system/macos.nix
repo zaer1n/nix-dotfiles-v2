59b37fdbb54c907020ec615478f241ab99ce5b77
zaer1n <105508453+zaer1n@users.noreply.github.com>
2025-08-10 21:09:48 +0300

    changes 5

--- modules/system/macos.nix
+++ modules/system/macos.nix
@@ -1,15 +1,44 @@
-{
+{ pkgs, ... }: {
   security.pam.services.sudo_local.touchIdAuth = true;
   system.defaults = {
     dock = {
+      tilesize = 48;
+      magnification = true;
+      largesize = 54;
       show-recents = false;
+      minimize-to-application = true;
+      persistent-apps = [
+        { app = "${pkgs.vscode}/Applications/Visual Studio Code.app"; }
+        { app = "/Applications/Ghostty.app"; }
+        { app = "/Applications/Zen.app"; }
+      ];
     };
+    
+    finder = {
+      ShowPathbar = true;
+      ShowStatusBar = true;
+      _FXShowPosixPathInTitle = false;
+    };
+
     trackpad = {
       Clicking = true;
     };
+
+    controlcenter = {
+      BatteryShowPercentage = true;
+    };
+
     NSGlobalDomain = {
+      AppleShowAllExtensions = true;
+
       "com.apple.swipescrolldirection" = false;
       "com.apple.mouse.tapBehavior" = 1;
+    };
+
+    CustomUserPreferences = {
+      "com.apple.Siri" = {
+        VoiceTriggerUserEnabled = 1;
+      };
     };
   };
 }
